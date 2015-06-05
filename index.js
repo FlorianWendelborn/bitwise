2790030ffc5aa91e62d208188d333050786ba091
Florian Wendelborn <florianwendgithub@gmail.com>
2015-06-05 20:23:06 +0200

    fixed style issues with codeclimate

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ for (var i = 0; i < 32; i++) {
 	p2[i] = Math.pow(2, i);
 }
 
+var readByteArray = [0,0,0,0,0,0,0,0];
 /**
  *	Returns an Array of length 8 containing the read bits.
  *
  *	@example
- *	readByte(42) → [0,0,1,0,1,0,1,0]
+ *	readByte(42) => [0,0,1,0,1,0,1,0]
  *
  *	@param data {Number} one byte of data
  *	@return {Array, false}
@@ -34,34 +35,35 @@ function readByte (data) {
 		return false;
 	}
 }
-var readByteArray = [0,0,0,0,0,0,0,0];
 
+var writeByteData = 0x00;
 /**
  *	Returns a Byte (0-255) which equals the given bits.
  *
  *	@example
- *	writeByte([0,0,1,0,1,0,1,0]) → 42
+ *	writeByte([0,0,1,0,1,0,1,0]) => 42
  *
  *	@param bits {Array} 8 bits to represent an 8 bit number
  *	@return {Number, false}
  */
 function writeByte (bits) {
 	if (typeof bits === 'object' && bits.length === 8) {
-		var data = 0x00;
+		writeByteData = 0x00;
 		for (var i = 0; i < 8; i++) {
-			data = data | (bits[i] ? p2[7-i] : 0);
+			writeByteData = writeByteData | (bits[i] ? p2[7-i] : 0);
 		}
-		return data;
+		return writeByteData;
 	} else {
 		return false;
 	}
 }
 
+var readBufferBits = [0,0,0,0,0,0,0,0];
 /**
  *	Returns an Array containing bitLength bits starting at bitOffset.
  *
  *	@example
- *	readBuffer(buffer, 2, 4) → [0,0,1,0]
+ *	readBuffer(buffer, 2, 4) => [0,0,1,0]
  *
  *	@param buffer {Buffer} the buffer to read
  *	@param offset {Number} where to start (in bits)
@@ -97,13 +99,12 @@ function readBuffer (buffer, offset, length) {
 	var subOffset = offset % 8;
 	return arr.slice(subOffset, subOffset+length);
 }
-var readBufferBits = [0,0,0,0,0,0,0,0];
 
 /**
  *	Modifies the buffer's bits to equal newBits starting at bitOffset.
  *
  *	@example
- *	modifyBuffer(buffer, [0,0,1,0], 0) → buffer was modified
+ *	modifyBuffer(buffer, [0,0,1,0], 0) => buffer was modified
  *
  *	@param buffer {Buffer} the buffer to modify
  *	@param bits {Array} the bits to insert
@@ -133,11 +134,12 @@ function modifyBuffer (buffer, bits, offset) {
 	}
 }
 
+var createBufferArray = [0,0,0,0,0,0,0,0];
 /**
  *	Creates a new buffer and writes the given bits.
  *
  *	@example
- *	createBuffer([1,1,1,1, 0,0,0,1, 1,0,1,0]) → buffer with data 1111 0001 1010 0000
+ *	createBuffer([1,1,1,1, 0,0,0,1, 1,0,1,0]) => buffer with data 1111 0001 1010 0000
  *
  *	@param bits {Array} an array containing the bits to insert
  *	@return {Buffer}
@@ -159,13 +161,14 @@ function createBuffer (bits) {
 
 	return buffer;
 }
-var createBufferArray = [0,0,0,0,0,0,0,0];
 
+var toBitsArray = [];
+var toBitsIndex = 0;
 /**
  *	Converts a string into an array of bits. Ignores all characters except 1 and 0.
  *
  *	@example
- *	toBits('10 10 12$%_.0') → [1,0,1,0,1,0]
+ *	toBits('10 10 12$%_.0') => [1,0,1,0,1,0]
  *
  *	@param string {String} the string to convert
  *	@return {Array}
@@ -185,14 +188,13 @@ function toBits (string) {
 
 	return toBitsArray;
 }
-var toBitsArray = [];
-var toBitsIndex = 0;
 
+var toStringString;
 /**
  *	Converts a bit array to a string. If defined, inserts spacer every spacing characters, but never inserts it as the last substring.
  *
  *	@example
- *	toString([1,0,1,0,1,0], 2, '_') → '10_10_10'
+ *	toString([1,0,1,0,1,0], 2, '_') => '10_10_10'
  *
  *	@param bits {Array} the bits to convert
  *	@param spacing {Number} where to place the spacers
@@ -219,14 +221,15 @@ function toString (bits, spacing, spacer) {
 		return bits.join('');
 	}
 }
-var toStringString;
 
+var readUIntArray = [];
+var readUIntResult = 0;
 /**
  *	Converts a section of a buffer to an unsigned integer.
  *  
  *	@example
  *	// buffer 11110110
- *	readUInt(buffer, 3, 5) → 22
+ *	readUInt(buffer, 3, 5) => 22
  *	
  *	@param buffer {Buffer} the buffer to extract information from
  *	@param length {Number} the length of the unsigned integer (in bits)
@@ -249,15 +252,15 @@ function readUInt (buffer, offset, length) {
 
 	return readUIntResult;
 }
-var readUIntArray = [];
-var readUIntResult = 0;
 
+var readIntArray = [];
+var readIntResult = 0;
 /**
  *	Converts a section of a buffer to a signed integer.
  *  
  *	@example
  *	// buffer 11110110
- *	readUInt(buffer, 3, 5) → -10
+ *	readUInt(buffer, 3, 5) => -10
  *	
  *	@param buffer {Buffer} the buffer to extract information from
  *	@param length {Number} the length of the signed integer (in bits)
@@ -288,8 +291,6 @@ function readInt (buffer, offset, length) {
 
 	return readIntResult;
 }
-var readIntArray = [];
-var readIntResult = 0;
 
 /**
  *	Converts a section of a buffer to a complementary integer.
@@ -297,7 +298,7 @@ var readIntResult = 0;
  *  
  *	@example
  *	// buffer 11110110
- *	readUInt(buffer, 3, 5) → -22
+ *	readUInt(buffer, 3, 5) => -22
  *	
  *	@param buffer {Buffer} the buffer to extract information from
  *	@param length {Number} the length of the signed integer (in bits)
