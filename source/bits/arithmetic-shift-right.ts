e94761f59ed0e67139c2e351f825fd4996b688d2
0xflotus <0xflotus@gmail.com>
2020-10-13 19:28:27 +0200

    cast

--- source/bits/arithmetic-shift-right.ts
+++ source/bits/arithmetic-shift-right.ts
@@ -4,7 +4,7 @@ import { Bit, Bits } from '../types'
  * Arithmetic Shift Right
  *
  * @example
- * ashiftr([1,0,1,1,0,1]) => [0,1,1,0,1,0]
+ * arithmeticShiftRight([1,0,1,1,0,1]) => [0,1,1,0,1,0]
  *
  * @param {Array} bits input data
  * @return {Array} [ASHIFTR bits]
@@ -13,7 +13,7 @@ export default (bits: Bits): Bits => {
     const result: Bits = []
 
     for (let i: number = 1; i < bits.length; i++) result.push(bits[i])
-    result.push(<Bit>0)
+    result.push(0 as Bit)
 
     return result
 }
