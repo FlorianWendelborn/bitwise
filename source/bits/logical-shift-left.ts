1dbe2f1d9ce8e889736502a793d52b1f95bf7f01
0xflotus <0xflotus@gmail.com>
2020-10-13 19:43:28 +0200

    renamed files

--- source/bits/logical-shift-left.ts
+++ source/bits/logical-shift-left.ts
@@ -6,7 +6,7 @@ import { Bit, Bits } from '../types'
  * Shifts all given bits to left and returns the resulting bits
  *
  * @example
- * lshiftl([1,0,1,1,0,1]) => [0,1,1,0,1,0]
+ * logicalShiftLeft([1,0,1,1,0,1]) => [0,1,1,0,1,0]
  *
  * @param {Array} bits input data
  * @return {Array} [LSHIFTL bits]
@@ -15,7 +15,7 @@ export default (bits: Bits): Bits => {
     const result: Bits = []
 
     for (let i: number = 0; i < bits.length - 1; i++) result.push(bits[i + 1])
-    result.push(<Bit>0)
+    result.push(0 as Bit)
 
     return result
 }
--- source/bits/logical-shift-right.ts
+++ source/bits/logical-shift-right.ts
@@ -6,7 +6,7 @@ import { Bit, Bits } from '../types'
  * Shifts all given bits to right and returns the resulting bits
  *
  * @example
- * lshiftr([1,0,1,1,0,1]) => [0,1,0,1,1,0]
+ * logicalShiftRight([1,0,1,1,0,1]) => [0,1,0,1,1,0]
  *
  * @param {Array} bits input data
  * @return {Array} [LSHIFTR bits]
@@ -14,7 +14,7 @@ import { Bit, Bits } from '../types'
 export default (bits: Bits): Bits => {
     const result: Bits = []
 
-    result.push(<Bit>0)
+    result.push(0 as Bit)
     for (let i: number = 0; i < bits.length - 1; i++) result.push(bits[i])
 
     return result
